69e5fbd2c4cdbfe2c3015e1a2e98c546145643bf
jessetello <jesse.tello@gmail.com>
2016-09-28 22:25:00 -0400

    fb login wip

--- TripStori/Pods/Target Support Files/Pods-TripStori/Pods-TripStori-acknowledgements.markdown
+++ TripStori/Pods/Target Support Files/Pods-TripStori/Pods-TripStori-acknowledgements.markdown
@@ -84,6 +84,10 @@ Copyright 2016 Google
 
 Copyright 2016 Google
 
+## FirebaseAuth
+
+Copyright 2016 Google
+
 ## FirebaseDatabase
 
 Copyright 2016 Google
--- TripStori/TripStori/TSWelcomeViewController.swift
+++ TripStori/TripStori/TSWelcomeViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FBSDKLoginKit
 
 class TSWelcomeViewController: UIViewController {
 
+    @IBOutlet weak var signIn: UIButton!
+    @IBOutlet weak var signUp: UIButton!
+    @IBOutlet weak var faceBookLogin: FBSDKLoginButton!
+    @IBOutlet weak var googleLogin: UIButton!
+
+    private let dataURL = "gs://tripstori-59fb9.appspot.com"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,5 +29,21 @@ class TSWelcomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func facebookLogin(_ sender: FBSDKLoginButton) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (loginResult, error) in
+            FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+            
+        }
+    }
+    
+    @IBAction func googleLogin(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    
 }
 
