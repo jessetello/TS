76d529959801cebc68808a72de845f18ac49f1f5
jessetello <jesse.tello@gmail.com>
2016-10-15 20:34:22 -0400

    record view wip

--- TripStori/TripStori/TSRecordViewController.swift
+++ TripStori/TripStori/TSRecordViewController.swift
@@ -11,6 +11,26 @@ import UIKit
 class TSRecordViewController: UIViewController {
    //This will eventually be custom recording view
     
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var addLocationButton: UIButton!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var descriptionTextView: UITextView!
     
+
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var seperatorLine: UIView!
+    @IBOutlet weak var initializingLabel: UILabel!
+    @IBOutlet weak var shareToFacebook: UIButton!
+    @IBOutlet weak var startStreamButton: UIButton!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    func setupCamera() {
+        
+    }
 }
